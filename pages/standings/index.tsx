7a61e9c26fb50f3c3941acd02d4c4704ed9690df
amir.mahdioun <amir.mahdioun@yahoo.com>
2022-11-26 02:38:05 +0330

    feat: standing page
    Add standing tables to standing page with header for group

--- pages/standings/index.tsx
+++ pages/standings/index.tsx
@@ -1,9 +1,41 @@
-const Standings = () => {
+import StandingTable from "../../modules/Standings/components/standingTable/StandingTable";
+import {Box, Container, Paper, Typography, useTheme} from "@mui/material";
+import {GetServerSideProps} from "next";
+import {getStandings} from "../../utils/api.util";
+import {StandingGroupI} from "../../interfaces/standingTable";
+
+type Props = {
+    standingData: StandingGroupI[]
+}
+const Standings = ({standingData}: Props) => {
+    const theme = useTheme()
+
     return (
-        <div>
-            Standings page
-        </div>
+        <Paper elevation={0}>
+            <Container maxWidth={'xl'}>
+                {
+                    standingData.map((group: StandingGroupI) => {
+                        return <Box my={'4rem'}>
+                            <Typography variant={'h4'}
+                                        mb={'1rem'}
+                                        textAlign={'center'}
+                                        color={theme.palette.primary.main}>Group {group.code}</Typography>
+                            <StandingTable rowData={group.teams}/>
+                        </Box>
+                    })
+                }
+            </Container>
+        </Paper>
     );
 };
+
+export const getServerSideProps: GetServerSideProps = async () => {
+    const data = await getStandings()
+    return {
+        props: {
+            standingData: data
+        }
+    }
+}
 
 export default Standings;
