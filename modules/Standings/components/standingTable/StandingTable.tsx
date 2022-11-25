4e3d4248cb852bbf66511b1a2a28fffdaba28494
amir.mahdioun <amir.mahdioun@yahoo.com>
2022-11-26 02:40:23 +0330

    feat: standing table
    Add standing table given data by props

---
+++ modules/Standings/components/standingTable/StandingTable.tsx
@@ -0,0 +1,102 @@
+import * as React from 'react';
+import {styled} from '@mui/material/styles';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell, {tableCellClasses} from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+import {StandingTeamsI} from "../../../../interfaces/standingTable";
+
+const StyledTableCell = styled(TableCell)(({theme}) => ({
+    [`&.${tableCellClasses.head}`]: {
+        color: theme.palette.common.white,
+        backgroundColor: theme.palette.primary.main
+    },
+    [`&.${tableCellClasses.body}`]: {
+        fontSize: 14,
+    }
+}));
+
+const StyledTableRow = styled(TableRow)(({theme}) => ({
+    // '&:nth-of-type(odd)': {
+    //     backgroundColor: theme.palette.action.hover,
+    // },
+    '&:last-child td, &:last-child th': {
+        border: 0,
+    }
+
+}));
+
+
+type Props = {
+    rowData: StandingTeamsI[]
+}
+
+export default function StandingTable({rowData}: Props) {
+    return (
+        <TableContainer elevation={5}
+                        component={Paper}
+                        sx={{borderRadius: '1rem', width: {xs: 300, sm: 500, md: 700, lg: 900}, marginX: 'auto'}}>
+            <Table sx={{minWidth: 700}}>
+                <TableHead>
+                    <TableRow>
+                        <StyledTableCell sx={{
+                            width: '2px',
+                            position: 'sticky',
+                            left: 0,
+                            boxShadow: "5px 2px 5px grey"
+                        }}></StyledTableCell>
+                        <StyledTableCell sx={{
+                            position: 'sticky',
+                            left: '15px',
+                            boxShadow: "5px 2px 5px grey"
+                        }}>Team</StyledTableCell>
+                        <StyledTableCell align="center">W</StyledTableCell>
+                        <StyledTableCell align="center">D</StyledTableCell>
+                        <StyledTableCell align="center">L</StyledTableCell>
+                        <StyledTableCell align="center">GF</StyledTableCell>
+                        <StyledTableCell align="center">GA</StyledTableCell>
+                        <StyledTableCell align="center">GD</StyledTableCell>
+                        <StyledTableCell align="center">Pts</StyledTableCell>
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                    {rowData.map((row: StandingTeamsI) => (
+                        <StyledTableRow key={row.id}
+                                        hover>
+                            <StyledTableCell component="th"
+                                             scope="row"
+                                             sx={{
+                                                 position: 'sticky',
+                                                 left: 0,
+                                                 boxShadow: "5px 2px 5px grey",
+                                                 backgroundColor: 'white'
+                                             }}>
+                                {row.position}
+                            </StyledTableCell>
+                            <StyledTableCell component="th"
+                                             scope="row"
+                                             sx={{
+                                                 position: 'sticky',
+                                                 left: '15px',
+                                                 boxShadow: "5px 2px 5px grey",
+                                                 backgroundColor: 'white'
+                                             }}>
+                                {row.alternateName}
+                            </StyledTableCell>
+                            <StyledTableCell align="center">{(row.points - row.draws) / 3}</StyledTableCell>
+                            <StyledTableCell align="center">{row.draws}</StyledTableCell>
+                            <StyledTableCell align="center">{row.losses}</StyledTableCell>
+                            <StyledTableCell align="center">{row.goalsScored}</StyledTableCell>
+                            <StyledTableCell align="center">{row.goalsConceded}</StyledTableCell>
+                            <StyledTableCell align="center">{row.goalsDifference}</StyledTableCell>
+                            <StyledTableCell align="center">{row.points}</StyledTableCell>
+                        </StyledTableRow>
+                    ))}
+                </TableBody>
+            </Table>
+        </TableContainer>
+    );
+}
