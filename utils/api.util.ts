8c0a56d9fdd31f5efbc95b38ae4f711f0e566f73
amir.mahdioun <amir.mahdioun@yahoo.com>
2022-11-26 02:36:43 +0330

    feat: get standings
    Add function to get standings data from an api

--- utils/api.util.ts
+++ utils/api.util.ts
@@ -7,21 +7,26 @@ export const loginUser = async () => {
 }
 
 export const getAllTeams = async (req: NextApiRequest, res: NextApiResponse) => {
-    const token = getCookie('token',{ req, res })
+    const token = getCookie('token', {req, res})
     const response = await fetch('http://api.cup2022.ir/api/v1/team', {
-        headers:{
-            'Authorization' : `Bearer ${token}`,
-            'Content-Type' : 'application/json'
+        headers: {
+            'Authorization': `Bearer ${token}`,
+            'Content-Type': 'application/json'
         }
     })
     return await response.json()
 }
 
 export const getGroups = async (req: NextApiRequest, res: NextApiResponse) => {
-    const {data} = await getAllTeams(req,res)
+    const {data} = await getAllTeams(req, res)
     return groupBy(data, (team) => {
         return team.groups
     })
+}
+
+export const getStandings = async () => {
+    const response = await fetch('https://copa22.medeiro.tech/groups')
+    return await response.json()
 }
 
 
