1528233f62fdbe347fead456ced243f633fa60e6
amir.mahdioun <amir.mahdioun@yahoo.com>
2022-11-28 15:35:50 +0330

    feat: all match interface
    add match interface for all matches

---
+++ interfaces/matches.ts
@@ -0,0 +1,65 @@
+interface PlayersI {
+    name: string,
+    number: number,
+    position: string
+}
+
+interface OfficialsI {
+    name: string,
+    role: string,
+    country: string
+}
+
+interface SubstitutionsI {
+    minute: string,
+    playerIn: {name: string, number: number, position: string},
+    playerOut: {name: string, number: number, position: string}
+}
+
+interface StaticsI {
+    attemptsOnGoal: number,
+    kicksOnTarget: number,
+    kicksOffTarget: number,
+    kicksBlocked: number,
+    kicksOnWoodwork: number,
+    corners: number,
+    offsides: number,
+    ballPossession: number,
+    passes: number,
+    passesCompleted: number,
+    distanceCovered: number,
+    freeKicks: number,
+    crosses: number,
+    crossesCompleted: number,
+    assists: number,
+    yellowCards: number,
+    redCards: number,
+    foulsCommited: number,
+    foulsReceived: number,
+    tactics: string,
+}
+
+interface TeamI {
+    country: string,
+    goals: number,
+    name: string,
+    penalties: number,
+    statistics: StaticsI,
+    substitutions: SubstitutionsI[],
+    startingPlayers: PlayersI[]
+}
+
+export interface MatchI {
+    id: string,
+    venue: string,
+    location: string,
+    status: string,
+    stageName: string,
+    time: string,
+    homeTeam: TeamI,
+    awayTeam: TeamI,
+    officials: OfficialsI[],
+    createdAt: string,
+    date: string,
+    updatedAt: string,
+}
