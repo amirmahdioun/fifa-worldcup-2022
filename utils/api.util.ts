c95f47c02e25f994fc2051375a4ffa1bff0706ff
amir.mahdioun <amir.mahdioun@yahoo.com>
2022-11-30 15:19:30 +0330

    feat: get today matches
    add custom function to get today matches by status of them

--- utils/api.util.ts
+++ utils/api.util.ts
@@ -1,6 +1,7 @@
 import {getCookie} from "cookies-next";
 import {NextApiRequest, NextApiResponse} from "next";
 import groupBy from 'lodash/groupBy'
+import {MatchI} from "../interfaces/matches";
 
 export const loginUser = async () => {
     const response = await fetch('http://api.cup2022.ir/api/v1/user/login')
@@ -29,9 +30,10 @@ export const getStandings = async () => {
     return await response.json()
 }
 
-export const getTodayMatches = async () => {
+export const getTodayMatches = async (status: 'All' | 'in_progress' | 'scheduled' | 'completed') => {
     const response = await fetch('https://copa22.medeiro.tech/matches/today')
-    return await response.json()
+    const data = await response.json()
+    return data.filter((item: MatchI) => item.status === status)
 }
 
 
