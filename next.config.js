864b44ad14a30e505b25d5792e92cf4d297a5d4f
amir.mahdioun <amir.mahdioun@yahoo.com>
2022-12-01 02:50:41 +0330

    pwa re-generated

--- next.config.js
+++ next.config.js
@@ -21,7 +21,7 @@
 const withPWA = require('next-pwa')({
   dest: 'public',
   mode: 'production',
-  disable: false,
+  disable: process.env.NODE_ENV === 'development',
 })
 
 module.exports = withPWA({
